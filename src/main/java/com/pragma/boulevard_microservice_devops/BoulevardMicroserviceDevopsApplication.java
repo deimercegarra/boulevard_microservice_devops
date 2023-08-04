039c715d6450d1b23fed8f01469291721ae60d4c
deimercegarra <deimer.cegarra@pragma.com.co>
2023-08-03 19:03:42 -0500

    devops

---
+++ src/main/java/com/pragma/boulevard_microservice_devops/BoulevardMicroserviceDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.boulevard_microservice_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulevardMicroserviceDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulevardMicroserviceDevopsApplication.class, args);
+	}
+
+}
